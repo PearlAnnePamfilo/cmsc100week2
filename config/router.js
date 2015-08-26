73b8dd4d33ae6a5ddc30957b751da94a67f047ce
PearlAnnePamfilo <pearlalala@gmail.com>
2015-08-26 12:29:55 +0800

    week2

--- config/router.js
+++ config/router.js
@@ -11,8 +11,13 @@ var teacher = require('./../controllers/teacher');
       return router;
 };*/
 module.exports = function(router){ //export functions on other modules
-      router.route('/students')
-            .get(student.find);  //callback function is transferred to controllers.student
+      router.route('/students') 
+            .get(student.find)  //callback function is transferred to controllers.student
+            .post(student.insert); //add a create function
+      router.route('/students/:id') //route handler, kailangan kasama yung id number sa URL
+            .get(student.findOne)
+            .put(student.update)
+            .delete(student.remove);
       router.route('/teachers')
             .get(teacher.find)
             .post(teacher.add);
